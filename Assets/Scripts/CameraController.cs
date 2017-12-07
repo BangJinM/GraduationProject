16c5989bcfb1c3e43693a57f994595da0ac8a3e8
马邦进 <mabangjin@outlook.com>
2017-12-07 21:31:18 +0800

    update

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject cameraObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) { }
+        else if(Input.GetMouseButtonDown(1)){
+            
+        }
+	}
+}
---
+++ Assets/Scripts/Entrance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entrance : MonoBehaviour {
+    UIControllerBase uIControllerBase;
+    // Use this for initialization
+    void Start () {
+        //界面--状态机
+        uIControllerBase = new UIControllerBase();
+
+        //主角
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        uIControllerBase.UIUpdate();
+	}
+}
